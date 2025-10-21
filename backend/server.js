10863ed9f06f527556963ed36a4f0d0bad133635
JaninduStellsync <janindu.stellsync@gmail.com>
2025-10-21 15:29:51 +0530

    adding latest changes

--- backend/server.js
+++ backend/server.js
@@ -995,11 +995,6 @@ app.get("/overlay/scorebar-tv", (_req, res) => {
 
 
 
-
-
-
-
-
 app.get("/overlay/scorebar-tv-orange", (_req, res) => { 
   res.set("Content-Type", "text/html; charset=utf-8").send(`<!doctype html>
 <html lang="en">
